159d4ea5b786e28dbe25cec2b67d21c664a3d098
William Roque <william.aroque@gmail.com>
2019-06-24 08:55:26 -0300

    Simplified package install.

---
+++ dependencies.bat
@@ -0,0 +1,5 @@
+ren "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Programs\Python\Python37\python.exe" python3.exe
+python3 -m pip install --upgrade pip
+python3 -m pip install pandas
+python3 -m pip install xlrd
+python3 -m pip install pycryptodome
